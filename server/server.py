cd74ac251cf5660a54e81fd1bc4b64468c2cd896
Blake Calhoun <blakecalhoun@tutanota.com>
2024-12-20 14:49:38 -0500

    added login test route

--- server/server.py
+++ server/server.py
@@ -124,6 +124,10 @@ def signup_archive():
 def login_page():
     return render_template('login.html')
 
+@app.route('/login_test', methods=['GET'])
+def login_page():
+    return render_template('login_test.html')
+
 # Create account
 @app.route('/signup', methods=['POST'])
 def signup():
